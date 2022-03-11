b60cf2bd21b3585b475ec982feab2103040b091a
Diego Peralta <diego.peralta@globant.com>
2022-03-11 00:31:33 -0300

    1.1
    Added the tab's name
    Created a const for the API authorization key
    Modified the description of the articles
    The likes are shown below the description of each one

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Formik, Form, Field } from 'formik'
 import './header.css'
 import './content.css'
@@ -7,6 +7,11 @@ import './article.css'
 const App = () => {
   const [photos, setPhotos ] = useState([])
   const open = url => window.open(url)
+  const authKey = 'INSERT_UNSPLASH_KEY_HERE'
+
+  useEffect(() => {
+    document.title = "IMG Search"
+  }, []);
 
   return (
     <div>
@@ -14,9 +19,9 @@ const App = () => {
         <Formik
           initialValues={{ search:''}}
           onSubmit={async values => {
-            const response = await fetch(`https://api.unsplash.com/search/photos?per_page=30&query=${values.search}`, { 
+            const response = await fetch(`https://api.unsplash.com/search/photos?per_page=28&query=${values.search}`, { 
               headers: {
-                'Authorization': 'Client-ID INSERT_UNSPLASH_KEY_HERE'
+                'Authorization': 'Client-ID '+authKey
               }
             })
             const data= await response.json()
@@ -31,9 +36,10 @@ const App = () => {
       <div className="container">
         <div className="center">
           {photos.map(photo =>
-            <article key={photo.id} onClick={()=> open(photo.links.html)}>
+            <article key={photo.id} onClick={()=> open(photo.urls.full)}>
               <img src={photo.urls.regular}/>
-              <p>{[photo.description, photo.alt_description].join(' - ')}</p>
+              <p>{photo.description ? photo.description : photo.alt_description}</p>
+              <p>&#128077; {photo.likes}</p>
             </article>)}
         </div>
       </div>
